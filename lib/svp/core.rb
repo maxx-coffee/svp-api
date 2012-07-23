de61e63a9897c60875ade5e979058e53a1031d73
Joshua Coffee <josh@joffeedesign.com>
2012-07-23 15:58:55 -0400

    adding get_video for testing

--- lib/svp/core.rb
+++ lib/svp/core.rb
@@ -40,6 +40,11 @@ class Core
         return resp
       end
 
+      def get_video(ref_no)
+        resp = get('svp_get_player_code', :query => {:video_ref => ref_no , :param_overrides => {:width = "947"} })
+        return resp
+      end
+
       def create_broadcast
         resp = get('svp_broadcast_feed')
         return resp
